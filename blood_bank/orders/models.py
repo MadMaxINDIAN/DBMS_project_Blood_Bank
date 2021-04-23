c57a00f8b0f27e012806d30f25c42d5bd03a1a42
Hiren Dhadhal <hirenhdhadhal23@gmail.com>
2021-04-23 20:02:00 +0530

    Order half

--- blood_bank/orders/models.py
+++ blood_bank/orders/models.py
@@ -1,18 +1,24 @@
 from django.db import models
+from hospital.models import *
+
+b_group_choices = (
+    ('A+', 'A+'),
+    ('A-', 'A-'),
+    ('B+', 'B+'),
+    ('B-', 'B-'),
+    ('AB+', 'AB+'),
+    ('AB-', 'AB-'),
+    ('O+', 'O+'),
+    ('O-', 'O-'),
+)
 
 # Create your models here.
 class Order (models.Model):
-    Hospital = models.CharField(max_length=100)
-    A_plus = models.CharField(max_length=10)
-    A_neg = models.CharField(max_length=10)
-    B_plus = models.CharField(max_length=10)
-    B_neg = models.CharField(max_length=10)
-    AB_plus = models.CharField(max_length=10)
-    AB_neg = models.CharField(max_length=10)
-    O_plus = models.CharField(max_length=10)
-    O_neg = models.CharField(max_length=10)
+    Hospital = models.ManyToManyField(Hospital ,null=False,blank=False)
+    blood_group = models.CharField(max_length=128, choices=b_group_choices)
+    quantity = models.IntegerField()
 
     
 
     def __str__(self):
-        return self.name
+        return self.Hospital.name
--- blood_bank/orders/views.py
+++ blood_bank/orders/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render, redirect
 from .form import *
+from .models import *
+from hospital.models import *
 
 # Create your views here.
 def orders(req):
-	form = CreateHospital()
-    context = {
-        'form': form
-    }
+    form = CreateOrder()
+    print(form.as_p)
     if req.method == 'POST':
-        form = CreateHospital(req.POST)
+        form = CreateOrder(req.POST)
         if form.is_valid():
-            form.save()
-            return redirect('/h/')
+            print(type(form.cleaned_data['Hospital'][0]))
+            order = Order(Hospital=form.cleaned_data['Hospital'][0],blood_group=form.cleaned_data['blood_group'],quantity=form.cleaned_data['quantity'] )
+            print(order)
+            return redirect('/o/')
         else:
             print('Form Invalid')
-    hospital = Hospital.objects.filter()
-    context['hospital'] = hospital
-    return render(req, 'orders.html', context=context)
+    orders = Order.objects.filter()
+    
+	# form = CreateOrder()
+    # context =  'form': form
+    # if req.method == 'POST':
+    #     form = CreateOrder(req.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('/o/')
+    #     else:
+    #         print('Form Invalid')
+    # orders = Order.objects.filter()
+    # context['orders'] = orders
+    return render(req, 'orders.html', context={'form':form.as_p,'orders':orders})
