32073497eee6784233f3e14e392be8a1e4da0fc7
MadMaxINDIAN <56634706+MadMaxINDIAN@users.noreply.github.com>
2021-04-24 12:30:36 +0530

    final

--- blood_bank/blood_bank_app/views.py
+++ blood_bank/blood_bank_app/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
 @login_required(login_url="/u/login_user")
 def home(req):
-    print(req.user)
     context = {
-        'A_plus': 23,
-        'A_neg': 17,
-        'B_plus': 30,
-        'B_neg': 45,
-        'AB_plus': 8,
-        'AB_neg': 15,
-        'O_plus': 18,
-        'O_neg': 32,
+        'A_plus': Blood_Stock.objects.get(blood_group='A+').quantity,
+        'A_neg': Blood_Stock.objects.get(blood_group='A-').quantity,
+        'B_plus': Blood_Stock.objects.get(blood_group='B+').quantity,
+        'B_neg': Blood_Stock.objects.get(blood_group='B-').quantity,
+        'AB_plus': Blood_Stock.objects.get(blood_group='AB+').quantity,
+        'AB_neg': Blood_Stock.objects.get(blood_group='AB-').quantity,
+        'O_plus': Blood_Stock.objects.get(blood_group='O+').quantity,
+        'O_neg': Blood_Stock.objects.get(blood_group='O-').quantity,
         'authenticated' : req.user.is_authenticated
     }
     return render(req, 'home.html', context=context)
