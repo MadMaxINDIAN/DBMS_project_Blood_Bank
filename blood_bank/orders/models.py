227a585d3d582a5ae3eabb70de4be31dc871d2fe
MadMaxINDIAN <56634706+MadMaxINDIAN@users.noreply.github.com>
2021-04-24 09:47:48 +0530

    Error solved

--- blood_bank/orders/models.py
+++ blood_bank/orders/models.py
@@ -14,11 +14,9 @@ b_group_choices = (
 
 # Create your models here.
 class Order (models.Model):
-    Hospital = models.ManyToManyField(Hospital ,null=False,blank=False)
+    Hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     blood_group = models.CharField(max_length=128, choices=b_group_choices)
     quantity = models.IntegerField()
 
-    
-
     def __str__(self):
-        return self.Hospital.name
+        return str(self.id)
--- blood_bank/orders/views.py
+++ blood_bank/orders/views.py
@@ -10,14 +10,11 @@ def orders(req):
     if req.method == 'POST':
         form = CreateOrder(req.POST)
         if form.is_valid():
-            print(type(form.cleaned_data['Hospital'][0]))
-            order = Order(Hospital=form.cleaned_data['Hospital'][0],blood_group=form.cleaned_data['blood_group'],quantity=form.cleaned_data['quantity'] )
-            print(order)
+            form.save()
             return redirect('/o/')
         else:
             print('Form Invalid')
     orders = Order.objects.filter()
-    
 	# form = CreateOrder()
     # context =  'form': form
     # if req.method == 'POST':
